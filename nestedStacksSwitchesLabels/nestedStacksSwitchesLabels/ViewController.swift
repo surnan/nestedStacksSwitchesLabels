39b6cf656daf9b7e4b8b1fff0387211aee227216
surnan <forsuresh@yandex.com>
2017-12-15 06:20:40 -0500

    first stack on screen.  Label needs to expand to include all text

--- nestedStacksSwitchesLabels/nestedStacksSwitchesLabels/ViewController.swift
+++ nestedStacksSwitchesLabels/nestedStacksSwitchesLabels/ViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var stack1 = UIStackView(); var label1 = UILabel(); var switch1 = UISwitch()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
         view.backgroundColor = UIColor.yellow
+        label1.text = "Hello World"
+        label1.textColor = .blue
+      
+        stack1 = UIStackView(arrangedSubviews: [label1, switch1])
+        
+        stack1.translatesAutoresizingMaskIntoConstraints = false
+        
+        stack1.axis = .vertical
+        stack1.distribution = .fill
+        stack1.spacing = 20
+        
+        view.addSubview(stack1)
+        
+        
+        stack1.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stack1.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+   
+        
+        
+        
         
     }
 }
